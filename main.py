ce021eb5d3547ec624049b59c033f768babbe8b9
parthojit <parthojit@yahoo.com>
2021-06-30 16:38:46 +0900

    add build

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 import tkinter as tk
 import serial
+import threading
+import time
+
+# % make command
+# % Command is a text chain formatted in the following way
+# % "pwd\nxxx\nxxx\nxxx\nxxx\nxxx\nxxx\nxxx\nxxx0\n"
+# % where xxx is the percentage on each valve
+# % the end 0 marks the end of the data stream
+# % The FPGA need this formating
 
 class Application(tk.Tk):
     def __init__(self):
@@ -8,20 +17,31 @@ class Application(tk.Tk):
         self.title('Mission Control')
         first_label = tk.Label(self,text="Odor blender",font=10)
         first_label.pack(pady=2,padx=2)
+        self.threads = []
  
     def design(self):
+        testbtn = tk.Button(self, text="TEST", width=25, command=self.test)
         startbtn = tk.Button(self, text="START", width=25, command=self.start)
-        measbtn = tk.Button(self, text="MEASURE", width=25, command=self.meas)
+        stopbtn = tk.Button(self, text="STOP", width=25, command=self.stop)
         quitbtn = tk.Button(self, text="QUIT", width=25, command=self.quit)
+        testbtn.pack()
         startbtn.pack()
-        measbtn.pack()
+        stopbtn.pack()
         quitbtn.pack()
 
-    def start(self):
+    def test(self):
         print("blender test")
    
-    def meas(self):
-        print("measurement start")
+    def start(self):
+        print("blender start")
+        self.meas_thread = threading.Thread(target=self.meas_proc,args=())
+        self.threads.append(self.meas_thread)
+        self.meas_thread.start()
+
+    def stop(self):
+        print("blender stop")
+        for thread in self.threads:
+            thread.join()
 
     def quit(self):
         print("quit")
@@ -41,6 +61,9 @@ class Application(tk.Tk):
     def run(self):
         self.design()
         self.mainloop()
+
+    def meas_proc(self):
+        print("process done")
 
 
 if __name__=="__main__":
